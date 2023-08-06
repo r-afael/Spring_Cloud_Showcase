aaf4a343ea5628ee881de46b065c02c3e2b95b38
r-afael <roliveira1111@gmail.com>
2023-08-05 21:09:52 -0300

    Implements Resilience4j for the Accounts Service

--- accounts/src/main/java/com/rafael/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/rafael/accounts/controller/AccountsController.java
@@ -4,38 +4,34 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.rafael.accounts.config.AccountsServiceConfig;
-import com.rafael.accounts.model.*;
-import com.rafael.accounts.repository.AccountRepository;
+import com.rafael.accounts.model.Account;
+import com.rafael.accounts.model.Customer;
+import com.rafael.accounts.model.CustomerDetails;
+import com.rafael.accounts.model.Properties;
 import com.rafael.accounts.service.AccountService;
-import com.rafael.accounts.client.CardsFeignClient;
-import com.rafael.accounts.client.LoansFeignClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import jakarta.el.PropertyNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 public class AccountsController {
     private final AccountsServiceConfig accountsConfig;
-    private final LoansFeignClient loansFeignClient;
-    private final CardsFeignClient cardsFeignClient;
     private final AccountService accountService;
-    private final AccountRepository accountRepository;
 
     @Autowired
-    public AccountsController(AccountService accountService, AccountRepository accountRepository,  AccountsServiceConfig accountsConfig,
-                              LoansFeignClient loansFeignClient, CardsFeignClient cardsFeignClient) {
+    public AccountsController(AccountService accountService,  AccountsServiceConfig accountsConfig) {
         this.accountService = accountService;
-        this.accountRepository = accountRepository;
         this.accountsConfig = accountsConfig;
-        this.loansFeignClient = loansFeignClient;
-        this.cardsFeignClient = cardsFeignClient;
     }
 
     /*
@@ -48,18 +44,17 @@ public class AccountsController {
      */
     @GetMapping("/myAccount")
     public ResponseEntity<?> getAccountByCustomerId(@RequestBody Customer customer) {
-        int customerId = customer.getCustomerId();
-        Optional<Account> accountOpt = accountService.getAccountByCustomerId(customerId);
-
-        if (accountOpt.isEmpty()) {
+        try {
+            Account account = accountService.getAccountByCustomerId(customer.getCustomerId());
+            return new ResponseEntity<>(account, HttpStatus.OK);
+        } catch (PropertyNotFoundException e) {
             Map<String, String> errorResponse = new HashMap<>();
-            errorResponse.put("message", "No account found for customer ID: " + customerId);
+            errorResponse.put("message", e.getMessage());
             return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-        } else {
-            return new ResponseEntity<>(accountOpt.get(), HttpStatus.OK);
         }
     }
 
+    //Gets remote env properties set on another repository
     @GetMapping("/account/properties")
     public String getPropertyDetails() throws JsonProcessingException {
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
@@ -70,35 +65,27 @@ public class AccountsController {
     }
 
     @PostMapping("/myCustomerDetails")
+    /*@Retry(name = "retryForCustomerDetails", fallbackMethod = "myCustomerDetailsFallBack")*/
+    @CircuitBreaker(name = "detailsForCustomerSupportApp", fallbackMethod = "customerDetailsFallback")
     public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
-        //Todo: Move relevant code to service class and return a ResponseEntity
-        Optional<Account> accountOpt = accountRepository.findByCustomerId(customer.getCustomerId());
-        if (accountOpt.isEmpty()) {
-            throw new PropertyNotFoundException("No account for customer with id: " + customer.getCustomerId());
-        }
-        Account account = accountOpt.get();
-
-        ResponseEntity<?> loansResponse = loansFeignClient.getLoansDetails(customer);
-        validateResponse(loansResponse);
-        List<Loan> loans = (List<Loan>) loansResponse.getBody();
-
-        ResponseEntity<?> cardsResponse = cardsFeignClient.getCardDetails(customer);
-        validateResponse(cardsResponse);
-        List<Card> cards = (List<Card>) cardsResponse.getBody();
-
-        CustomerDetails customerDetails = new CustomerDetails();
-        customerDetails.setAccount(account);
-        customerDetails.setLoans(loans);
-        customerDetails.setCards(cards);
-
-        return customerDetails;
+        //Todo: return a ResponseEntity and add try catch
+        return accountService.getCustomerDetails(customer);
     }
 
-    private void validateResponse(ResponseEntity<?> response) {
-        if (!response.getStatusCode().is2xxSuccessful()) {
-            Map<String, String> errorResponse = (Map<String, String>) response.getBody();
-            String errorMessage = errorResponse.get("message");
-            throw new PropertyNotFoundException(errorMessage);
-        }
+    //Tests Rate Limiter to protect APIs from too many requests, you can try this by refreshing the page multiple times
+    @GetMapping("/sayHello")
+    @RateLimiter(name = "sayHello", fallbackMethod = "sayHelloFallback")
+    public String sayHello() {
+        return "Hello, this is the standard return";
+    }
+
+    //Fallback method called from the Circuit Breaker
+    private CustomerDetails customerDetailsFallback(Customer customer, Throwable t) {
+        return accountService.customerDetailsFallback(customer, t);
+    }
+
+    //Fallback method called from the Rate Limiter
+    private String sayHelloFallback(Throwable t) {
+        return "Hi, this is the rate limiter fallback";
     }
 }
--- accounts/src/main/java/com/rafael/accounts/service/AccountService.java
+++ accounts/src/main/java/com/rafael/accounts/service/AccountService.java
@@ -1,24 +1,82 @@
 package com.rafael.accounts.service;
 
-import com.rafael.accounts.model.Account;
+import com.rafael.accounts.client.CardsFeignClient;
+import com.rafael.accounts.client.LoansFeignClient;
+import com.rafael.accounts.model.*;
 import com.rafael.accounts.repository.AccountRepository;
+import jakarta.el.PropertyNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class AccountService {
-
     private final AccountRepository accountRepository;
+    private final LoansFeignClient loansFeignClient;
+    private final CardsFeignClient cardsFeignClient;
 
     @Autowired
-    public AccountService(AccountRepository accountRepository) {
+    public AccountService(AccountRepository accountRepository, LoansFeignClient loansFeignClient, CardsFeignClient cardsFeignClient) {
         this.accountRepository = accountRepository;
+        this.loansFeignClient = loansFeignClient;
+        this.cardsFeignClient = cardsFeignClient;
     }
 
-    public Optional<Account> getAccountByCustomerId(int customerId) {
-        return accountRepository.findByCustomerId(customerId);
+    public Account getAccountByCustomerId(int customerId) {
+        Optional<Account> accountOpt = accountRepository.findByCustomerId(customerId);
+        return accountOpt.orElseThrow(() -> new PropertyNotFoundException("No account for customer with id: " + customerId));
+    }
+
+    //Makes request to the Loads Service using Feign
+    public List<Loan> getLoansDetails(Customer customer) {
+        ResponseEntity<?> loansResponse = loansFeignClient.getLoansDetails(customer);
+        validateResponse(loansResponse);
+        return (List<Loan>) loansResponse.getBody();
+    }
+
+    //Makes request to the Cards Service using Feign
+    public List<Card> getCardsDetails(Customer customer) {
+        ResponseEntity<?> cardsResponse = cardsFeignClient.getCardDetails(customer);
+        validateResponse(cardsResponse);
+        return (List<Card>) cardsResponse.getBody();
+    }
+
+    public CustomerDetails getCustomerDetails(Customer customer) {
+        Account account = getAccountByCustomerId(customer.getCustomerId());
+        List<Loan> loans = getLoansDetails(customer);
+        List<Card> cards = getCardsDetails(customer);
+
+        CustomerDetails customerDetails = new CustomerDetails();
+        customerDetails.setAccount(account);
+        customerDetails.setLoans(loans);
+        customerDetails.setCards(cards);
+
+        return customerDetails;
+    }
+
+    //Dummy method to test a fallback for when Cards Service is not available, making calls only to Loans Service
+    public CustomerDetails customerDetailsFallback(Customer customer, Throwable t) {
+        Account account = this.getAccountByCustomerId(customer.getCustomerId());
+        List<Loan> loans = this.getLoansDetails(customer);
+
+        CustomerDetails customerDetails = new CustomerDetails();
+        customerDetails.setAccount(account);
+        customerDetails.setLoans(loans);
+
+        return customerDetails;
+    }
+
+    //Validates successful response
+    private void validateResponse(ResponseEntity<?> response) {
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            Map<String, String> errorResponse = (Map<String, String>) response.getBody();
+            String errorMessage = errorResponse.get("message");
+            throw new PropertyNotFoundException(errorMessage);
+        }
     }
 
 }
